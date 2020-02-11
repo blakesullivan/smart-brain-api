ca948342cd46e0528a01af08013c4f8efa090163
blakesullivan <blake.r.sullivan@gmail.com>
2020-02-11 16:28:33 +1100

    Initial commit

---
+++ controllers/image.js
@@ -0,0 +1,28 @@
+const Clarifai = require('clarifai');
+
+//Must be declared to use the api.
+const cApp = new Clarifai.App({
+  apiKey: '19f206fda880438b8177186a389063a2'
+});
+
+const handleApiCall = (req, res) => {
+	cApp.models
+		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+		.then(data => {
+			res.json(data);
+		})
+		.catch(err => res.status(400).json('Unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0]);
+	})
+	.catch(err => res.status(400).json('Unable to get Entries'))
+}
+
+module.exports = {handleImage, handleApiCall}
